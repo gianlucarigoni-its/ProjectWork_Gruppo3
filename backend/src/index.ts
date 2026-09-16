71e44d3c4475fae4d193e76e636c890fc05150d5
gianlucarigoni-its <gianluca.rigoni@allievi.itsdigitalacademy.com>
2026-09-16 12:19:38 +0200

    connessione atlas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,16 @@ import "reflect-metadata";
 import { createServer } from "http";
 import app from "./app";
 import mongoose from "mongoose";
+import "dotenv/config";
+
+const dbPassword = process.env.DB_PASSWORD;
+if (!dbPassword) throw new Error("DB_PASSWORD non configurata nel file .env");
 
 mongoose.set("debug", true);
 mongoose
-  .connect("mongodb://localhost:27017/projecwork")
+  .connect(`mongodb+srv://gianlucarigoni_db_user:${encodeURIComponent(dbPassword)}` + `@bankinappdb.wiep7c5.mongodb.net/bankinapp`)
   .then((_) => {
+    console.log("MongoDB connected");
     createServer(app).listen(3000, () => {
       console.log("Server listening on port 3000");
     });
