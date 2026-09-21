86a7e304a8b76791fcbf465beaf58708997771a7
gianlucarigoni-its <gianluca.rigoni@allievi.itsdigitalacademy.com>
2026-09-21 11:45:34 +0200

    .

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -1,21 +1,12 @@
 import { Router } from "express";
 import accountsRouter from "./accounts/accounts.router";
-<<<<<<< HEAD
-import { RicaricaController } from '../api/ricarica/ricarica.controller';
-import { authMiddleware } from '../middleware/auth-middleware';
-import { BonificoController } from './ricarica/bonifco.controller';
-=======
-import transactionsRouter from "./transactions/transaction.router";
->>>>>>> 0029d5f (.)
+import { RicaricaController } from "../api/ricarica/ricarica.controller";
+import { authMiddleware } from "../middleware/auth-middleware";
+import { BonificoController } from "./ricarica/bonifco.controller";
 
 const apiRouter = Router();
 
-<<<<<<< HEAD
-apiRouter.post('/operations/recharge',authMiddleware, RicaricaController.handleRicarica);
-apiRouter.post('/operations/transfer', authMiddleware, BonificoController.handleBonifico);
-=======
-router.use("/accounts", accountsRouter);
-router.use("/transactions", transactionsRouter);
->>>>>>> 0029d5f (.)
+apiRouter.post("/operations/recharge", authMiddleware, RicaricaController.handleRicarica);
+apiRouter.post("/operations/transfer", authMiddleware, BonificoController.handleBonifico);
 
 export default apiRouter;
